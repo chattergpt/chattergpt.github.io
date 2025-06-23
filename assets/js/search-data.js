4e7074a5e424aebad1f62833580a0ccd9703aa25
chattergpt <48930666+chattergpt@users.noreply.github.com>
2025-06-23 23:04:59 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@22a68a0a7a67c3e514f815a48fb2c50e1cb20957 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%63%68%61%74%74%65%39@%75.%72%6F%63%68%65%73%74%65%72.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/chattergpt", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +516,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/sreejato-chatterjee", "_blank");
         },
       },{
         id: 'social-rss',
@@ -523,13 +537,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
