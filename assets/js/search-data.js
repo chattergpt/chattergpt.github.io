5909ee87a940cd3c9fe9cf1d5f3bdb7f64956e95
chattergpt <48930666+chattergpt@users.noreply.github.com>
2025-06-24 03:54:21 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@d0bf6016ea208036d78c3150fd091f476a21b133 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -392,9 +392,9 @@ ninja.data = [{
           description: "🏆 1st Place - Undergraduate Division",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-2025-raise-informatics-data-science-amp-ai-competition",
+          title: '2025 RAISE Informatics, Data Science &amp;amp; AI Competition',
+          description: "🏆 2nd Place - Undergraduate Division",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
