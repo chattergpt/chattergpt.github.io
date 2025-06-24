62bad602400a1eec5fb04d9717c3333a23f03e2f
chattergpt <48930666+chattergpt@users.noreply.github.com>
2025-06-24 04:15:38 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@584e39ecb2e219886246e7ebcce8a2be69c5ab3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,9 +397,9 @@ ninja.data = [{
           description: "🏆 2nd Place - Undergraduate Division",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-2025-rit-asa-datafest",
+          title: '2025 RIT ASA Datafest',
+          description: "🏆 1st Place - Best Visualization",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
