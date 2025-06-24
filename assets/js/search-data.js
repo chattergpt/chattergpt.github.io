4927f7120c91b49a3cdd7d9624d5634f86da3a06
chattergpt <48930666+chattergpt@users.noreply.github.com>
2025-06-24 03:22:43 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@5713b63ffb7bebee19af6c22528201a0069feb32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,9 +387,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-2024-uofr-biomedical-data-science-hackathon",
+          title: '2024 UofR Biomedical Data Science Hackathon',
+          description: "🏆 1st Place - Undergraduate Division",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
