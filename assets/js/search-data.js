cb7a7bff4c90e8ec656d0cef247a1442aaabd13b
chattergpt <48930666+chattergpt@users.noreply.github.com>
2026-01-14 18:21:44 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@e1fb819c664abca1fefe770505bf4dc9e78c15e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -151,17 +151,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/geojson-map/";
           
         },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
