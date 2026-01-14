ad5a2297cc5501fa0629f84da7d8e851deed8a02
chattergpt <48930666+chattergpt@users.noreply.github.com>
2026-01-14 19:05:31 +0000

    Deploying to gh-pages from @ chattergpt/chattergpt.github.io@2660a80368ed5ef3560f0b7fc4a3e4cf8f83b574 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,28 +63,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
-      },{id: "post-a-post-with-tabs",
-        
-          title: "a post with tabs",
-        
-        description: "this is what included tabs in a post could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/tabs/";
-          
-        },
-      },{id: "post-a-post-with-typograms",
-        
-          title: "a post with typograms",
-        
-        description: "this is what included typograms code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/typograms/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
